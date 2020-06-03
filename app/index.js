da6591669adc359f74d32e5e026a58d34d8af80f
Yuliana Hazda <yulianahazda@gmail.com>
2020-06-03 16:42:18 -0400

    Added and removed classes from modals on different events

--- app/index.js
+++ app/index.js
@@ -1,141 +1,130 @@
-// Select: (modal left/right, startButton, intro, userInput)
-
-const modalLeft = $(".modalLeft");
-const modalRight = $(".modalRight");
-const intro = $('.intro');
-const buttonStart = $('.buttonStart');
+// Create a namespace
+const quizApp = {};
 
 // Create an array of objects with question and correct answer
-
-const quiz = [
-    { question: "Enter shortcut command to create a new file ?", answer: ["Meta", 'n'] },
-    { question: "Enter shortcut command to insert backtick notation `` ?", answer: ["Control", '`'] },
-    { question: "Enter shortcut command to find anything in VScode ?", answer: ["Meta", 'f'] },
-    { question: "Enter shortcut command to open file ?", answer: ["Meta", 'o'] },
-    { question: "Enter shortcut command - quick open ?", answer: ["Meta", 'p'] }
+quizApp.quiz = [
+    { question: "Enter shortcut command to create a new file ?", answer: ["Meta", "n"], userKeys: [], },
+    { question: "Enter shortcut command to insert backtick notation `` ?", answer: ["Control", "`"], userKeys: [], },
+    { question: "Enter shortcut command to find anything in VScode ?", answer: ["Meta", "f"], userKeys: [], },
+    { question: "Enter shortcut command to open file ?", answer: ["Meta", "o"], userKeys: [], },
+    { question: "Enter shortcut command - quick open ?", answer: ["Meta", "p"], userKeys: [], }
 ];
+
+
+quizApp.animateModal = (i) => {
+    // Remove width from modal left/right to hide it (or animate)
+    $(".modalRight").animate({
+        width: 0
+    });
+
+    $(".modalLeft").animate({
+        width: 0
+    });
+
+    // Hide introduction text
+    $(".intro").hide();
+
+    // Load first quiz question on pressing strat button
+    $("h2").text(`${quizApp.quiz[i].question}`);
+}
+
+quizApp.keyChecker = (key) => {
+    // In a key placeholder I passed e.key argument
+    // Change the default value of META & SPACE keys 
+    if (key === "Meta") {
+        key = "cmd";
+    }
+
+    if (key === "Space") {
+        key = "space";
+    }
+}
+
+
+quizApp.userInput = (input) => {
+    // Push user inputs into "userKeys" array
+}
+
+quizApp.updateCounter = (val) => {
+    const counterValue = $(".counter").text(`${val}`);
+    return counterValue;
+}
+
+quizApp.openModalScore = (num) => {
+    const myArr = quizApp.quiz;
+    if ((myArr.length - 1) === num) {
+
+        // Display: none modal intro when game is over
+        $('.modalScore').removeClass('close');
+
+        // Add animation
+        setTimeout(function () {
+            $(".modalScoreLeft").animate({
+                left: "0"
+            }, 1000);
+
+            $(".modalScoreRight").animate({
+                right: "0"
+            }, 1000);
+        })
+
+        // Wait for animation to be finished then display score window
+        setTimeout(function () {
+            $('.scoreTotal').show(2000, function () {
+                $(this).css({ "top": "15%" });
+            });
+        }, 2000)
+    }
+}
+
+quizApp.init = () => {
+    let counter = 0;
+    $('.modalScore').addClass('close');
+
+
+
+    $(".buttonStart").on("click", function (e) {
+        // prevent link default behaviour 
+        e.preventDefault();
+
+        // index starts at 0
+        quizApp.animateModal(counter);
+    });
+
+
+    // Listen "document" object on the keydown event 
+    $(document).on("keydown", function (e) {
+        // Prevent browser default behavior 
+        if (e.key === "s" || e.key === "o" || e.key === "f" || e.key === "p") {
+            e.preventDefault();
+        }
+
+        quizApp.keyChecker(e.key);
+        quizApp.userInput(e.key)
+    });
+
+
+
+
+    $(".buttonNext").on("click", function (e) {
+        e.preventDefault();
+
+        $("h2").text(`${quizApp.quiz[++counter].question}`);
+        
+        // Clears up user input
+        $(".userInput1").text("");
+        $(".userInput2").text("");
+
+        quizApp.updateCounter((counter + 1));
+        quizApp.openModalScore(counter);
+    })
+}
 
 
 
 
 
 $(function () {
-    buttonStart.on('click', function (e) {
-         // prevent link default behaviour 
-         e.preventDefault();
- 
-         // Remove width from modal left/right to hide it (or animate)
-         $(modalRight).animate({
-             width: 0
-         });
-         $(modalLeft).animate({
-             width: 0
-         });
- 
-         // Hide introduction text
-         $(intro).hide();
-     }) 
+    quizApp.init();
+})
 
-
-
-    // Listen "document" object on the keydown event 
-    let userKeys = [];
-
-    $(document).on('keydown', function (e) {
-        // Prevent browser default behavior 
-        if (e.key === 's' || e.key === 'o' || e.key === 'f' || e.key === 'p' || e.key === 'n') {
-            e.preventDefault();
-        }
-
-        // Change the default value of META & SPACE keys 
-        if (e.key === "Meta") {
-            e.key = 'cmd';
-        }
-
-        if (e.code === "Space") {
-            e.key = "space";
-        }
-
-        // Push user inputs into array
-        userKeys.push(e.key);
-
-        // Update html with first user input
-        let firstInput = $('.userInput1').text(userKeys[0]);
-        let secondInput = $('.userInput2').text(userKeys[1]);
-
-        if (userKeys.length > 2) {
-            userKeys = [];
-            userKeys.push(e.key);
-            firstInput.text(userKeys[0]);
-            secondInput.text(userKeys[1]);
-        }
-    });
-
-
-
-
-    let counter = 0;
-    let counter2 = 1;
-
-    const updateCounter = (val) => {
-        $('.counter').text(`${val}`);
-    }
-
-    let innerHtml = $('h2').text(`${quiz[counter].question}`);
-
-    $('.buttonNext').on('click', function (e) {
-        e.preventDefault();
-        innerHtml = $('h2').text(`${quiz[++counter].question}`);
-        $('.userInput1').text('');
-        $('.userInput2').text('');
-        updateCounter(++counter2);
-    })
-});
-
-
-
-// for (let i = 0; i < quiz.length; i++) {
-//     console.log(quiz[i].answer[i]);
-//     for (let l = 0; l < userKeys.length; l++) {
-//         if (quiz[i].answer[i] === userKeys[l]) {
-//             console.log(true);
-//         } 
-//     }
-// }
-
-
- // Create a namespace
-        // Create an array of objects with a question and correct answer
-        // Select: (modal left/right, startButton, intro, userInput)
-
-        // Modal-intro pseudo-code (on click event):
-        // prevent link default behaviour 
-        // Remove width from modal left/right to hide it (or animate)
-        // Hide introduction text
-
-
-
-        // Quiz game pseudo-code :
-        // Listen "document" on keydown event
-        // Pass (event) object as an argument to the callback
-        // Prevent default behaviour 
-        // Prevent browser shortcuts default behaviour
-        // Create an array "userKeys" = [] inside of "quiz"
-        // Store user input in an array "userKeys"
-        // Update DOM with the user input dynamically 
-        // Create new object "score"
-        // Loop through the quiz array 
-        // While looping compare (with if statement) "userKeys" and "answer"
-        // Store each "question" and (true/false) answer in a "score" object
-        // Update modal window DOM with the user inputs
-
-        // Add click event:
-        // prevent link default behaviour 
-        // add counter
-        // On click event update counter (add 1) each time user hit "next" button
-        // Update HTML "counter" dynamically
-        // Update question (on click is should display next question)
-        // Keep track of last question with if statement
-        // When last question was answered pop up modal window (addClass('open')) with the user score
-
-        // On page load call init method
